b8ad26b94f9ef2a998e7719ccbb69e3a5412f1c1
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-19 00:35:08 +0530

    animation added

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -1,10 +1,22 @@
+import { motion } from "framer-motion";
 import { SubTitle } from "./PageTitle";
 
 export default function TimelineSection({ title, data }) {
   return (
-    <>
+    <motion.div
+      initial={{ y: 300, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0, duration: 0.8 }}
+      viewport={{ once: true, amount: 0 }}
+    >
       <SubTitle text={title} />
-      <ol className="w-11/12 md:w-5/6 lg:w-4/6 pt-4 relative border-s border-highlight2 dark:border-highlight">
+      <motion.ol
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0, duration: 0.8 }}
+        viewport={{ once: true, amount: 0 }}
+        className="w-11/12 md:w-5/6 lg:w-4/6 pt-4 relative border-s border-highlight2 dark:border-highlight"
+      >
         {data.map((data, index) => (
           <li className="mb-10 ms-4" key={index}>
             <div className="absolute w-3 h-3 bg-highlight2 dark:bg-highlight rounded-full mt-1.5 -start-1.5 border border-highlight2 dark:border-highlight"></div>
@@ -21,7 +33,7 @@ export default function TimelineSection({ title, data }) {
             </div>
           </li>
         ))}
-      </ol>
-    </>
+      </motion.ol>
+    </motion.div>
   );
 }
