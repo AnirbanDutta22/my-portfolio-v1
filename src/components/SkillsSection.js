056e62909819bd4ada5c55f2a2c0ef17ffb62929
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-19 00:35:54 +0530

    animation and skills icons added

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -1,40 +1,22 @@
+import { motion } from "framer-motion";
 import bootstrap_icon from "../assets/images/bootstrap_icon.png";
 import CSS_icon from "../assets/images/css_icon.png";
 import express_icon from "../assets/images/express_icon.png";
 import HTML_icon from "../assets/images/html5_icon.png";
 import JS_icon from "../assets/images/javascript_icon.png";
 import mongodb_icon from "../assets/images/mongodb_icon.png";
+import next_icon from "../assets/images/next_icon.png";
 import node_icon from "../assets/images/nodejs_icon.png";
 import react_icon from "../assets/images/react_icon.png";
-import sass_icon from "../assets/images/sass_icon.png";
 import tailwind_css_icon from "../assets/images/tailwind_css_icon.png";
+import typescript_icon from "../assets/images/typescript_icon.png";
 import wordpress_icon from "../assets/images/wordpress_icon.png";
 import { SubTitle } from "./PageTitle";
 
 const skills_icon_table = [
   {
-    name: "HTML",
-    link: HTML_icon,
-  },
-  {
-    name: "CSS",
-    link: CSS_icon,
-  },
-  {
-    name: "JS",
-    link: JS_icon,
-  },
-  {
-    name: "Bootstrap",
-    link: bootstrap_icon,
-  },
-  {
-    name: "Sass",
-    link: sass_icon,
-  },
-  {
-    name: "Tailwind",
-    link: tailwind_css_icon,
+    name: "Next.js",
+    link: next_icon,
   },
   {
     name: "React.js",
@@ -53,6 +35,30 @@ const skills_icon_table = [
     link: mongodb_icon,
   },
   {
+    name: "Tailwind",
+    link: tailwind_css_icon,
+  },
+  {
+    name: "Typescript",
+    link: typescript_icon,
+  },
+  {
+    name: "JS",
+    link: JS_icon,
+  },
+  {
+    name: "HTML",
+    link: HTML_icon,
+  },
+  {
+    name: "CSS",
+    link: CSS_icon,
+  },
+  {
+    name: "Bootstrap",
+    link: bootstrap_icon,
+  },
+  {
     name: "Wordpress",
     link: wordpress_icon,
   },
@@ -60,7 +66,12 @@ const skills_icon_table = [
 
 export default function SkillsSection() {
   return (
-    <>
+    <motion.div
+      initial={{ y: 300, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0, duration: 0.8 }}
+      viewport={{ once: true, amount: 0 }}
+    >
       <SubTitle text="skills" />
       <div className="w-full md:w-11/12 pt-4 mb-5 text-base grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-14 items-center">
         {skills_icon_table.map((icons, index) => (
@@ -73,6 +84,6 @@ export default function SkillsSection() {
           </div>
         ))}
       </div>
-    </>
+    </motion.div>
   );
 }
